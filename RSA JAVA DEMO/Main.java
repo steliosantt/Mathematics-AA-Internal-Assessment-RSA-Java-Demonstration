fff2cd87c5abe8737b3e705fe338d54afdc7abc2
steliosantt <82447387+steliosantt@users.noreply.github.com>
2024-07-16 23:06:46 -0400

    Add files via upload

---
+++ RSA JAVA DEMO/Main.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args){
+        
+        while(true){
+            System.out.println("\nEnter number for Alice to send to Bob (enter -1 to exit): ");
+            Scanner in = new Scanner(System.in);
+            double message = in.nextDouble();
+            if(message == -1) break;
+            double c = Alice.encrypt(message);
+            System.out.println("After encryption the message that Alice sends to Bob is: "+c);
+            double nm = Bob.send_and_decrypt(c);
+            System.out.println("After decryption from Bob the message is: "+(int)nm);            
+        }
+    }
+}
