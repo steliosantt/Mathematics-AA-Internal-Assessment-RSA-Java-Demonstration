fe339fe2394776420e906789d5d4ee2aa44aca38
steliosantt <82447387+steliosantt@users.noreply.github.com>
2024-07-16 23:14:25 -0400

    Update Main.java

--- RSA JAVA DEMO/Main.java
+++ RSA JAVA DEMO/Main.java
@@ -1,9 +1,7 @@
 
 /**
- * Write a description of class Driver here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Main Class.
+ * Stylianos N Antoniadis, 2024
  */
 import java.util.*;
 
